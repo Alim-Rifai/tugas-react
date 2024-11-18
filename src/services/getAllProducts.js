1ebf706ee2801a74b7b7468a1814f4cec0f93b2a
Alim-Rifai <alim22068si@student.nurulfikri.ac.id>
2024-11-18 22:19:29 +0700

    nyoba

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,65 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'NAF-1-07-FMEN-001',
+      slug: 'Backpack Tas Ransel Polos Hitam-men',
+      name: "Backpack Tas Ransel Polos Hitam",
+      category: `Men's Shoes`,
+      categorySlug: 'men-shoes',
+      price: 1_549_000,
+      stock: 100,
+      imageUrl: '/assets/images/Backpack Tas Ransel Polos Hitam.jpeg',
+      description: `Tas Ransel Hitam polos,,
+      sangat pas buat yang mau jualan dan bisa di sablon sendiri dan pasang logo sendiri..
+      - Bahan Tebal
+      - Kwalitas Terjamin
+      - Tidak Ada kantong Kiri kanan.
+      - Harga Pabrik.
+      - Cocok Buat Cewek cowok.
+      - Ukuran 40 x 30 x 12 cm.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-002',
+      slug: 'DAILY BACKPACK-men',
+      name: "DAILY BACKPACK",
+      category: `Men's Shoes`,
+      categorySlug: 'men-shoes',
+      price: 1_909_000,
+      stock: 5,
+      imageUrl: `/assets/images/DAILY BACKPACK.jpeg`,
+      description: `Detail Produk
+      Tipe: TAS RANSEL
+      Bahan: Waterproof Cordura
+      Warna: Black 
+      Sizing: Adult Backpack
+
+      Feature:
+      - Waterproof material
+      - Cushioned laptop compartment
+      - Luggage strap
+      - Anti-theft pockets
+      - 2x Water bottle compartment
+
+      Size Chart: Tinggi x Panjang x Lebar`
+    },
+    {
+      id: 'NAF-1-07-FWOMEN-003',
+      slug: 'Tas Selempang Wanita Import-women',
+      name: "Tas Selempang Wanita Import",
+      category: `Women's Shoes`,
+      categorySlug: 'women-shoes',
+      price: 1_909_000,
+      stock: 0,
+      imageUrl: `/assets/images/Tas Selempang Wanita Import.jpg`,
+      description: `Tas Selempang atau hand bag ini Kini hadir dikoleksi toko CILPI, memenuhi kebutuhan fashion anda
+      dengan bahan waterproof dan terbuat dengan bahan 100% high quality. cocok digunakan diberbagai acara 
+      pastinya dikombinasikan dengan koleksi pakaian anda  
+
+      panjang  : 31 cm 
+      Lebar     : 14 cm 
+      Tinggi    : 23 cm 
+      Bahan   : PU `
+    },
+
+  ]
+}
